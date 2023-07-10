9053c38e74d989efb87b3135cc892a409fe05847
lusenakalee <lusenakalee@gmail.com>
2023-07-10 19:04:04 +0300

    activity form

--- src/components/ActivityComps/ActivityForm.js
+++ src/components/ActivityComps/ActivityForm.js
@@ -1,14 +1,108 @@
 import React from "react";
-import { Form, json, redirect } from "react-router-dom";
+import {
+  Form,
+  json,
+  redirect,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 import { getAuthToken } from "../../util/Auth";
 
 function ActivityForm({ activity, method, title }) {
+
+  const navigate = useNavigate();
+  const navigation = useNavigation();
+  const data = useActionData();
+  const isSubmitting = navigation.state === "submitting";
+  function cancelHandler() {
+    navigate("..");
+  }
+
+
+
+
   return (
     <React.Fragment>
-        {title}
-        <Form method={method}>
+      {title}
+      <Form method={method}>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Activity name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-        </Form>
+          <div className="col-span-full">
+            <label
+              htmlFor="about"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Activity description
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600">
+              Write a few sentences about yourself.
+            </p>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="date"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Event Date
+            </label>
+            <div className="mt-2">
+              <input
+                id="date"
+                name="date"
+                type="date"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-between sm:col-span-4">
+              <button
+                onClick={cancelHandler}
+                className="px-4 py-2 text-sm font-medium text-gray-900 bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                Back
+              </button>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="px-4 py-2 text-sm font-medium text-gray-900 bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                {isSubmitting ? "Submitting..." : "Save"}
+              </button>
+            </div>
+
+
+
+        </div>
+      </Form>
     </React.Fragment>
   );
 }
--- src/pages/ActivityPages/ActivityMainPages/MainActivityRoot.js
+++ src/pages/ActivityPages/ActivityMainPages/MainActivityRoot.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import ActivityMainNav from '../../../components/ActivityComps/ActivityMainNav'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import ActivityMainNav from "../../../components/ActivityComps/ActivityMainNav";
+import { Outlet } from "react-router-dom";
 
 function MainActivityRoot() {
   return (
     <React.Fragment>
-        <ActivityMainNav/>
-        <Outlet/>
+      <ActivityMainNav />
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
     </React.Fragment>
-  )
+  );
 }
 
-export default MainActivityRoot
+export default MainActivityRoot;
