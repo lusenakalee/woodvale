597bf5ab848a409f57fcabf14bc4219023c4981b
lusenakalee <lusenakalee@gmail.com>
2023-07-20 13:18:50 +0300

    delete build

--- build/static/js/1606.ec9eab55.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwoodvale=self.webpackChunkwoodvale||[]).push([[1606],{1606:function(e,n,r){r.r(n);var s=r(2791),t=r(7304),l=r(7689),a=r(184);n.default=function(){var e=(0,l.V4)("root"),n=(0,l.V4)("root").user;return(0,a.jsxs)(s.Fragment,{children:[(0,a.jsx)(s.Suspense,{fallback:(0,a.jsx)("p",{style:{textAlign:"center"},children:"Loading...."}),children:(0,a.jsx)(l.KP,{resolve:n,children:function(n){return e&&(0,a.jsx)(t.Z,{userData:n})}})}),(0,a.jsx)(l.j3,{})]})}}}]);
-//# sourceMappingURL=1606.ec9eab55.chunk.js.map
