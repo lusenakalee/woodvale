cee6ef0f5a158686ccc2bb06edd23ce41380d9c2
Brian <brianmugz1@gmail.com>
2023-07-26 08:58:17 +0300

    nav changes

--- src/components/RootComps/Nav.js
+++ src/components/RootComps/Nav.js
@@ -5,12 +5,6 @@ import { Form, Link } from "react-router-dom";
 import { getAuthToken } from "../../util/Auth";
 import abstractUser from "../../components/assets/images/abstractUser.png"
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
   { name: "Dashboard", to: "/login/home", current: true },
   { name: "Staff", to: "./staff", current: false },
